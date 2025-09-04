488651b4d8594b17dbf21aa7fecdb4bbbc38a5c7
1abdullah-saad1 <abd.saad.mahmod@gmail.com>
2025-09-05 02:26:39 +0300

    intialize user views

---
+++ app/Livewire/Comment/Thread.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Comment;
+
+use Livewire\Component;
+
+class Thread extends Component
+{
+    public function render()
+    {
+        return view('livewire.comment.thread');
+    }
+}
