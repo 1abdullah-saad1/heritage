ddf5ea32de8e5ce60ff72a019065f25bfb52c80b
1abdullah-saad1 <abd.saad.mahmod@gmail.com>
2025-09-05 02:25:20 +0300

    initiallize expert views

---
+++ app/Livewire/Expert/EndorseModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Expert;
+
+use Livewire\Component;
+
+class EndorseModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.expert.endorse-modal');
+    }
+}
