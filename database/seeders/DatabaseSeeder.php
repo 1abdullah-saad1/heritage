009fbf7475aea7a2aa54b24bfb1530717a4d6ed5
1abdullah-saad1 <abd.saad.mahmod@gmail.com>
2025-09-05 03:06:46 +0300

    chore(seeder): update DatabaseSeeder to call RolesSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    {// مثال Breeze الافتراضي:
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'username' => 'testuser', // ⬅️ أضِف هذا
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call(RolesSeeder::class);
     }
 }
