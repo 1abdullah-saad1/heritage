23fa1f6d4cfb904834d379449336cf4f4b1b2202
1abdullah-saad1 <abd.saad.mahmod@gmail.com>
2025-09-05 03:05:47 +0300

    feat(model): implement relations (media, comments, likes, hashtags, endorsements, reviews) and casts in Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,61 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    /** @use HasFactory<\Database\Factories\PostFactory> */
-    use HasFactory;
+    protected $fillable = ['user_id', 'body', 'status', 'verification_state', 'category_id', 'location_id', 'license', 'meta', 'approved_at', 'rejected_reason', 'endorse_count'];
+
+    protected $casts = ['meta' => 'array', 'approved_at' => 'datetime'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function media()
+    {
+        return $this->hasMany(PostMedia::class)->orderBy('order');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function hashtags()
+    {
+        return $this->belongsToMany(Hashtag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function endorsements()
+    {
+        return $this->hasMany(ExpertEndorsement::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function reports()
+    {
+        return $this->morphMany(Report::class, 'target');
+    }
 }
